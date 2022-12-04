372df514cfbeefe8be4e4c090e9c1df6c88a1ba2
Luken <github@luken.cc>
2022-12-04 15:07:27 +0100

    :recycle: refactor code duplication for day04

--- src/day04/main.rs
+++ src/day04/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let input = fs::read_to_string("./input/day04.txt").unwrap();
     let re = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
 
-    let part_1 = input
+    let processed = input
         .split("\n")
         .filter(|line| !line.is_empty())
         .map(|line| match re.captures(line) {
@@ -19,39 +19,20 @@ fn main() {
                 ))
             }
             None => None,
-        }.unwrap())
+        }.unwrap()).collect::<Vec<_>>();
+
+    let part_1 = processed.iter()
         .map(|(left, right)| {
             if (left.contains(&right.start()) && left.contains(&right.end())) ||
-                (right.contains(&left.start()) && right.contains(&left.end())) {
-                1
-            } else {
-                0
-            }
+                (right.contains(&left.start()) && right.contains(&left.end())) { 1 } else { 0 }
         })
         .sum::<i32>();
 
     println!("Part1: {:?}", part_1);
 
-    let part_2 = input
-        .split("\n")
-        .filter(|line| !line.is_empty())
-        .map(|line| match re.captures(line) {
-            Some(caps) => {
-                let extract = |index: usize| -> i32 { caps.get(index).unwrap().as_str().parse::<i32>().unwrap() };
-                Some((
-                    extract(1) ..= extract(2),
-                    extract(3) ..= extract(4)
-                ))
-            }
-            None => None,
-        }.unwrap())
+    let part_2 = processed.iter()
         .map(|(left, right)| {
-            if (left.contains(&right.start()) || left.contains(&right.end())) ||
-                (right.contains(&left.start()) || right.contains(&left.end())) {
-                1
-            } else {
-                0
-            }
+            if left.start() <= right.end() && right.start() <= left.end() { 1 } else { 0 }
         })
         .sum::<i32>();
 
